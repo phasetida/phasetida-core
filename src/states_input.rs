9ab0d354d12aa5868ed15b8d11c9c6bcde970a46
nofyso <1052470899@qq.com>
2026-02-19 14:50:13 +0800

    feat: re-added touch

---
+++ src/states_input.rs
@@ -0,0 +1,28 @@
+use crate::TOUCH_STATES;
+
+/// Set a touch point as enabled
+pub fn set_touch_down(id: usize, x: f32, y: f32) {
+    TOUCH_STATES.with_borrow_mut(|it| {
+        it.get_mut(id).map(|touch| {
+            touch.touch_down(x, y);
+        });
+    });
+}
+
+/// Move a touch point
+pub fn set_touch_move(id: usize, x: f32, y: f32) {
+    TOUCH_STATES.with_borrow_mut(|it| {
+        it.get_mut(id).map(|touch| {
+            touch.touch_move(x, y);
+        });
+    })
+}
+
+/// Set a touch point as disabled
+pub fn set_touch_up(id: usize) {
+    TOUCH_STATES.with_borrow_mut(|it| {
+        it.get_mut(id).map(|touch| {
+            touch.touch_up();
+        });
+    })
+}
